8cac917c7ec9273109f8c0c824d86e2c4c0a31a2
lgmclaughlin <lgmclaughlin@wpi.edu>
2013-10-05 21:06:33 -0700

    It appears to work :D

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 all: meh_malloc1
 
 meh_malloc1: meh_malloc1.c
-	gcc -o meh_malloc1 meh_malloc1.c
+	gcc -g -o meh_malloc1 meh_malloc1.c
+
+clean:
+	rm -rf meh_malloc1 *.o
--- meh_malloc1.c
+++ meh_malloc1.c
@@ -10,7 +10,7 @@ void *malloc_head = NULL;
 int malloc_buffer_size = 0;
 
 void *meh_malloc(int size) {
-	void *start = NULL;
+	void *start = malloc_head;
 
 	if (malloc_head == NULL) {
 		int malloc_size = MAX(size + 8, SBRK_SIZE);
@@ -24,7 +24,7 @@ void *meh_malloc(int size) {
 		}
 	}
 
-	while (*(int *)start != 0) {
+	while (((int *)start)[0] != 0) {
 		// Set start to be the second 4 bytes of the current start
 		// This is the pointer to the next item in the linked list
 		start = (void *) ((int *) start)[1];
@@ -43,7 +43,7 @@ void *meh_malloc(int size) {
 	// The first 4 bytes are the size of the memory malloc'd
 	((int *) start)[0] = size;
 	// The second 4 bytes are a pointer to the next chunk of memory
-	((int *) start)[1] = (int) start + size + 4;
+	((int *) start)[1] = (int) start + size + 8;
 
 	return &((int *) start)[2];
 }
@@ -60,5 +60,9 @@ int main(int argc, char *argv[]) {
 	printf("Value of malloc_head: %p\n", malloc_head);
 	printf("Value of malloc_buffer_size: %i\n", malloc_buffer_size);
 	printf("Value of abc: %p\n", abc);
+	void *def = meh_malloc(64);
+	printf("Value of malloc_head: %p\n", malloc_head);
+	printf("Value of malloc_buffer_size: %i\n", malloc_buffer_size);
+	printf("Value of def: %p\n", def);
 	exit(0);
 }
