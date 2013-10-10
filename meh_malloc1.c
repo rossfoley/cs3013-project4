fe02518d0e4b9e2d5e55bbaa14b107dc4b88d36a
lgmclaughlin <lgmclaughlin@wpi.edu>
2013-10-09 19:38:27 -0700

    Started to restructure linked list

--- meh_malloc1.c
+++ meh_malloc1.c
@@ -3,17 +3,26 @@
 #include <unistd.h>
 #include <error.h>
 
-#define SBRK_SIZE 1024
-#define MAX(a,b) a > b ? a : b
+#define SBRK_SCALE 100
+#define CHECKSUM ~0
 
 void *malloc_head = NULL;
 int malloc_buffer_size = 0;
 
+/*
+4 bytes each:
+1. Free Space Size
+2. Next Pointer
+3. Prev Pointer
+4. Used Space Size
+5. Checksum
+*/
+
 void *meh_malloc(int size) {
 	void *start = malloc_head;
+	int malloc_size = size * SBRK_SCALE;
 
 	if (malloc_head == NULL) {
-		int malloc_size = MAX(size + 8, SBRK_SIZE);
 		malloc_head = sbrk(malloc_size);
 
 		if (malloc_head == (void *) -1) {
@@ -29,10 +38,7 @@ void *meh_malloc(int size) {
 		// This is the pointer to the next item in the linked list
 		start = (void *) ((int *) start)[1];
 		if (start >= (malloc_head + malloc_buffer_size - (size + 8))) {
-			int malloc_size = MAX(size + 8, SBRK_SIZE);
-			int result = (int) sbrk(malloc_size);
-
-			if (result == -1) {
+			if ((int) sbrk(malloc_size) == -1) {
 				return NULL;
 			} else {
 				malloc_buffer_size += malloc_size;
@@ -53,21 +59,29 @@ void meh_free(void *abcdefg) {
 }
 
 int main(int argc, char *argv[]) {
+	printf("Initial Values\n");
 	printf("Value of malloc_head: %p\n", malloc_head);
 	printf("Value of malloc_buffer_size: %i\n", malloc_buffer_size);
-	printf("Trying to meh_malloc 64 bytes!\n");
+	printf("\n");
+	printf("Trying to meh_malloc 64 bytes.\n");
 	void *abc = meh_malloc(64);
 	printf("Value of malloc_head: %p\n", malloc_head);
 	printf("Value of malloc_buffer_size: %i\n", malloc_buffer_size);
 	printf("Value of abc: %p\n", abc);
+	printf("\n");
+	printf("Trying to meh_malloc 128 more bytes.\n");
 	void *def = meh_malloc(128);
 	printf("Value of malloc_head: %p\n", malloc_head);
 	printf("Value of malloc_buffer_size: %i\n", malloc_buffer_size);
 	printf("Value of def: %p\n", def);
+	printf("\n");
+	printf("Trying to meh_malloc 1024 more bytes.\n");
 	void *ghi = meh_malloc(1024);
 	printf("Value of malloc_head: %p\n", malloc_head);
 	printf("Value of malloc_buffer_size: %i\n", malloc_buffer_size);
 	printf("Value of ghi: %p\n", ghi);
+	printf("\n");
+	printf("Trying to meh_malloc 1234 more bytes.\n");
 	void *jkl = meh_malloc(1234);
 	printf("Value of malloc_head: %p\n", malloc_head);
 	printf("Value of malloc_buffer_size: %i\n", malloc_buffer_size);
